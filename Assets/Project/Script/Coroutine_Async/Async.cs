3ead0036c7d853c38dc9424c7589531956e3e9d8
vincentgohzy <vincentgoh1982@gmail.com>
2021-06-07 14:39:42 +0800

    Async vs Coroutine information and example

---
+++ Assets/Project/Script/Coroutine_Async/Async.cs
@@ -0,0 +1,43 @@
+﻿using NaughtyAttributes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Async : MonoBehaviour
+{
+    public Text myTextField;
+    private CancellationTokenSource source;
+
+    [Button]
+    public void Press()
+    {
+        source = new CancellationTokenSource();
+        WriteAsync(source.Token);
+        WriteForLoop(" For_Loop ");
+    }
+    [Button]
+    public void Stop()
+    {
+        source.Cancel();
+        myTextField.text = myTextField.text + " StopAsync " + " , time: " + Time.realtimeSinceStartup;
+    }
+
+    private async void WriteAsync(CancellationToken token)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            myTextField.text = myTextField.text + " StartAsync " + i + " , time: " + Time.realtimeSinceStartup;
+            await Task.Delay(TimeSpan.FromSeconds(3), token);
+        }
+    }
+
+    private void WriteForLoop(string word)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            myTextField.text = myTextField.text + (string)word + i + " , time: " + Time.realtimeSinceStartup;
+        }
+    }
+}
---
+++ Assets/Project/Script/Coroutine_Async/Coroutine.cs
@@ -0,0 +1,43 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coroutine : MonoBehaviour
+{
+    public Text myTextField;
+    private IEnumerator coroutine;
+
+    [Button]
+    public void Press()
+    {
+        coroutine = WriteCoroutine(" StartCoroutine: ");
+        StartCoroutine(coroutine);
+        WriteForLoop(" For_Loop ");
+    }
+
+    [Button]
+    public void Stop_Coroutine()
+    {
+        StopCoroutine(coroutine);
+        myTextField.text = myTextField.text + " StopCoroutine: " + Time.realtimeSinceStartup;
+    }
+
+    IEnumerator WriteCoroutine(string word)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            myTextField.text = myTextField.text + (string)word + i + " , time: " + Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    private void  WriteForLoop(string word)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            myTextField.text = myTextField.text + (string)word + i + " , time: " + Time.realtimeSinceStartup;
+        }
+    }
+}
