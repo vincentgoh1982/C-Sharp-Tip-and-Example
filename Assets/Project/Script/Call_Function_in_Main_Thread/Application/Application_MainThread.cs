7dde213b872908a4b63e73a96910466940112274
vincentgohzy <71680463+vincentgohzy@users.noreply.github.com>
2021-10-01 20:55:54 +0800

    Missing script

---
+++ Assets/Project/Script/Call_Function_in_Main_Thread/Application/Application_MainThread.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element_MainThread : MonoBehaviour
+{
+    public Application_MainThread app
+    {
+        get
+        {
+            return GameObject.FindObjectOfType<Application_MainThread>();
+        }
+    }
+}
+
+
+public class Application_MainThread : MonoBehaviour
+{
+    public Controller_Network controller;
+    public Model_Network model;
+    public View_MainThread view;
+    public Controller_OtherToMainThread controllerMainThread;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
