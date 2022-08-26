28e903680dd2ef7e708a9f4f34c0bcc5e6b78656
VG <goh.zhengyang.vincent@stengg.com>
2022-08-26 13:02:17 +0800

    Added Unity client to server code

---
+++ Assets/Project/Script/TCP_Beginner/ClientApplication.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ClientElement : MonoBehaviour
+{
+    // Gives access to the application and all instances.
+    public ClientApplication app
+    {
+        get
+        {
+            return GameObject.FindObjectOfType<ClientApplication>();
+        }
+    }
+}
+
+public class ClientApplication : MonoBehaviour
+{
+    public TcpClient client;
+    public ClientModel model;
+    public ClientController controller;
+
+    private void Start()
+    {
+        string path = Application.dataPath + "/StreamingAssets";
+
+        if (Directory.Exists(path))
+        {
+            Debug.Log($"It exists inside this directory: {path}");
+        }
+        else
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+}
---
+++ Assets/Project/Script/TCP_Beginner/ClientModel.cs
@@ -0,0 +1,6 @@
+﻿
+public class ClientModel : ClientElement
+{
+    public string serverIp = "172.16.1.216";
+    public int port = 1302;
+}
