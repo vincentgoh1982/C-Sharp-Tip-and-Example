169eb3fa69b8279e3831e4cce7720f5a545c9abd
vincentgoh1982 <71366416+vincentgoh1982@users.noreply.github.com>
2023-11-24 10:13:29 +0800

    Create UnityMainThreadDispatcher .cs
    Unity Main Thread Dispatcher:
    A dispatcher that allows you to queue actions to be executed on Unity's main thread.

---
+++ Assets/Project/Script/UDP_Client_Chat/UnityMainThreadDispatcher .cs
@@ -0,0 +1,19 @@
+public class UnityMainThreadDispatcher : MonoBehaviour {
+    private static readonly Queue<Action> _executionQueue = new Queue<Action>();
+
+    public static void Enqueue(Action action) {
+        lock (_executionQueue) {
+            _executionQueue.Enqueue(action);
+        }
+    }
+
+    void Update() {
+        while (_executionQueue.Count > 0) {
+            Action action;
+            lock (_executionQueue) {
+                action = _executionQueue.Dequeue();
+            }
+            action?.Invoke();
+        }
+    }
+}
